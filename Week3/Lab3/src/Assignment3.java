aa85fd679c6e47e36a2a845436b852c767b05dfd
Stephen ByeongJun Kim <stbykim@iu.edu>
2020-09-09 01:54:19 +0900

    Test

--- Week3/Lab3/src/Assignment3.java
+++ Week3/Lab3/src/Assignment3.java
@@ -1,3 +1,28 @@
 public class Assignment3 {
-  
+
+    static class Tea {
+        double bird;
+        void book(double watch) {
+            this.bird = (watch - this.bird);
+        }
+        Tea (double snake) {
+            this.bird = snake * 2;
+        }
+    }
+    public static void main(String[ ] args) {
+        Tea  m = new Tea(-2);
+        Tea  n = new Tea(42);
+
+        m.book(92);
+        n.bird = m.bird + n.bird;
+        System.out.println(n.bird);
+
+        n.book(25);
+        System.out.println(n.bird);
+
+        n.book(25);
+        System.out.println(n.bird);
+    }
 }
+
+
